27059c22b4ae0e1c721ce4bdb3818058bbbc5d04
Torben <torben.christians@outlook.de>
2021-08-24 10:58:25 +0200

    Added Database Syntax

--- new_project/src/components/Table.vue
+++ new_project/src/components/Table.vue
@@ -7,7 +7,7 @@
 
           <v-data-table
       :headers="headers"
-      :items="desserts"
+      :items="links"
       :search="search"
       sort-by="calories"
       class="elevation-1"
@@ -30,7 +30,7 @@
 
           <v-dialog
             v-model="dialog"
-            max-width="500px"
+            max-width="1000px"
           >
             <template v-slot:activator="{ on, attrs }">
               <v-btn
@@ -57,7 +57,7 @@
                       md="4"
                     >
                       <v-text-field
-                        v-model="editedItem.name"
+                        v-model="editedItem.from"
                         label="Target-URL"
                       ></v-text-field>
                     </v-col>
@@ -77,8 +77,8 @@
                       md="4"
                     >
                       <v-text-field
-                        v-model="editedItem.fat"
-                        label="Fat (g)"
+                        v-model="editedItem.to"
+                        label="Destination-URL"
                       ></v-text-field>
                     </v-col>
                     <v-col
@@ -87,8 +87,8 @@
                       md="4"
                     >
                       <v-text-field
-                        v-model="editedItem.carbs"
-                        label="Carbs (g)"
+                        v-model="editedItem.domain"
+                        label="Domain"
                       ></v-text-field>
                     </v-col>
                     <v-col
@@ -96,10 +96,6 @@
                       sm="6"
                       md="4"
                     >
-                      <v-text-field
-                        v-model="editedItem.protein"
-                        label="Protein (g)"
-                      ></v-text-field>
                     </v-col>
                   </v-row>
                 </v-container>
@@ -183,29 +179,27 @@
                 text: 'Target-URL',
                 align: 'start',
                 //sortable: false,
-                value: 'name',
+                value: 'from',
             },
             //{ text: 'Calories', value: 'calories' },
-            { text: 'Destination-URL', value: 'fat' },
-            { text: 'Domain', value: 'carbs' },
-            { text: 'created', value: 'protein' },
+            { text: 'Destination-URL', value: 'to' },
+            { text: 'Domain', value: 'domain' },
+            { text: 'created', value: 'created' },
             { text: '', value: 'actions', sortable: false },
             ],
-            desserts: [],
+            links: [],
             editedIndex: -1,
             editedItem: {
-            name: '',
-            calories: 0,
-            fat: 0,
-            carbs: 0,
-            protein: 0,
+            from: '',
+            to: '',
+            domain: '',
+            created: '',
             },
             defaultItem: {
-            name: '',
-            calories: 0,
-            fat: 0,
-            carbs: 0,
-            protein: 0,
+            from: '',
+            to: '',
+            domain: '',
+            created: '',
             },
         }),
 
@@ -230,94 +224,30 @@
 
         methods: {
             initialize () {
-            this.desserts = [
+            this.links = [
                 {
-                name: 'Frozen Yogurt',
-                calories: 159,
-                fat: 6.0,
-                carbs: 24,
-                protein: 4.0,
-                },
-                {
-                name: 'Ice cream sandwich',
-                calories: 237,
-                fat: 9.0,
-                carbs: 37,
-                protein: 4.3,
-                },
-                {
-                name: 'Eclair',
-                calories: 262,
-                fat: 16.0,
-                carbs: 23,
-                protein: 6.0,
-                },
-                {
-                name: 'Cupcake',
-                calories: 305,
-                fat: 3.7,
-                carbs: 67,
-                protein: 4.3,
-                },
-                {
-                name: 'Gingerbread',
-                calories: 356,
-                fat: 16.0,
-                carbs: 49,
-                protein: 3.9,
-                },
-                {
-                name: 'Jelly bean',
-                calories: 375,
-                fat: 0.0,
-                carbs: 94,
-                protein: 0.0,
-                },
-                {
-                name: 'Lollipop',
-                calories: 392,
-                fat: 0.2,
-                carbs: 98,
-                protein: 0,
-                },
-                {
-                name: 'Honeycomb',
-                calories: 408,
-                fat: 3.2,
-                carbs: 87,
-                protein: 6.5,
-                },
-                {
-                name: 'Donut',
-                calories: 452,
-                fat: 25.0,
-                carbs: 51,
-                protein: 4.9,
-                },
-                {
-                name: 'KitKat',
-                calories: 518,
-                fat: 26.0,
-                carbs: 65,
-                protein: 7,
+                from: 'Frozen Yogurt',
+                to: 159,
+                domain: 6.0,
+                created: 24,
                 },
             ]
             },
 
             editItem (item) {
-            this.editedIndex = this.desserts.indexOf(item)
+            this.editedIndex = this.links.indexOf(item)
             this.editedItem = Object.assign({}, item)
             this.dialog = true
             },
 
             deleteItem (item) {
-            this.editedIndex = this.desserts.indexOf(item)
+            this.editedIndex = this.links.indexOf(item)
             this.editedItem = Object.assign({}, item)
             this.dialogDelete = true
             },
 
             deleteItemConfirm () {
-            this.desserts.splice(this.editedIndex, 1)
+            this.links.splice(this.editedIndex, 1)
             this.closeDelete()
             },
 
@@ -339,9 +269,9 @@
 
             save () {
             if (this.editedIndex > -1) {
-                Object.assign(this.desserts[this.editedIndex], this.editedItem)
+                Object.assign(this.links[this.editedIndex], this.editedItem)
             } else {
-                this.desserts.push(this.editedItem)
+                this.links.push(this.editedItem)
             }
             this.close()
             },
