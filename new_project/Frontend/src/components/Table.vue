aeb1970060eaecdca504e06295f73622cd78910d
Torben <torben.christians@outlook.de>
2021-08-25 12:00:56 +0200

    Complete Refactoring of file structure, server missed fix

---
+++ new_project/Frontend/src/components/Table.vue
@@ -0,0 +1,308 @@
+<template>
+  <!-- <v-container>  -->
+      
+      
+      <div class="table">
+          <h1>Hello I am the Database tab</h1>
+
+          <v-data-table
+      :headers="headers"
+      :items="links"
+      :search="search"
+      sort-by="Target-URL"
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <v-toolbar
+          flat
+        >
+
+<!-- //SEARCH -->
+          <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+
+        <v-spacer></v-spacer>
+
+          <v-dialog
+            v-model="dialog"
+            max-width="1000px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="primary"
+                dark
+                class="mb-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                Add Redirect
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">{{ formTitle }}</span>
+              </v-card-title>
+  
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.from"
+                        label="Target-URL"
+                      ></v-text-field>
+                    </v-col>
+                    <!-- <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.calories"
+                        label="Calories"
+                      ></v-text-field>
+                    </v-col> -->
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.to"
+                        label="Destination-URL"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-select
+                      :items="items"
+                        v-model="editedItem.domain"
+                        label="Domain"
+                      ></v-select>
+
+
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+  
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="close"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="save"
+                >
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-icon
+          small
+          class="mr-2"
+          @click="editItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+          small
+          @click="deleteItem(item)"
+        >
+          mdi-delete
+        </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn
+          color="primary"
+          @click="reset"
+        >
+          Reset
+        </v-btn>
+      </template>
+    </v-data-table>
+
+
+      </div> 
+      
+      
+      <!-- </v-container> -->
+</template>
+
+<script>
+
+  
+
+  export default {
+    name: 'Table',
+
+    data: () => ({
+       db:require("../dblogic/dbreq"),
+       items: ['rittal.de', 'loh-services.com', 'lkh.de', 'loh-academy.com', 'stahlo.de'],
+
+            search: '',
+            dialog: false,
+            dialogDelete: false,
+            headers: [
+            {
+                text: 'Target-URL',
+                align: 'start',
+                //sortable: false,
+                value: 'from',
+            },
+            //{ text: 'Calories', value: 'calories' },
+            { text: 'Destination-URL', value: 'to' },
+            { text: 'Domain', value: 'domain' },
+            { text: 'created', value: 'created' },
+            { text: '', value: 'actions', sortable: false },
+            ],
+
+            links: [],
+
+            editedIndex: -1,
+            editedItem: {
+            from: '',
+            to: '',
+            domain: '',
+            created: '',
+            },
+            defaultItem: {
+            from: '',
+            to: '',
+            domain: '',
+            created: '',
+            },
+        }),
+
+        computed: {
+            formTitle () {
+            return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+            },
+        },
+
+        watch: {
+            dialog (val) {
+            val || this.close()
+            },
+            dialogDelete (val) {
+            val || this.closeDelete()
+            },
+        },
+
+        async created () {
+            
+            this.links = await this.initalize();
+        },
+
+        methods: {
+
+             async initalize(){
+              var links = await this.db.getLinks()
+              return new Promise((resolve) => {
+                resolve(links)
+              })
+            },
+
+            reset(){
+              return this.links
+            },
+            
+
+            editItem (item) {
+            this.editedIndex = this.links.indexOf(item)
+            this.editedItem = Object.assign({}, item)
+            this.dialog = true
+            },
+
+            deleteItem (item) {
+            this.editedIndex = this.links.indexOf(item)
+            this.editedItem = Object.assign({}, item)
+            this.dialogDelete = true
+            },
+
+            deleteItemConfirm () {
+            this.db.deleteLink(this.links[this.editedIndex].id)
+            this.links.splice(this.editedIndex, 1)
+            this.closeDelete()
+            },
+
+            close () {
+            this.dialog = false
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            })
+            },
+
+            closeDelete () {
+            this.dialogDelete = false
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            })
+            },
+
+            async save () {
+
+            if(this.editedItem.from == this.defaultItem.from || this.editedItem.to == this.defaultItem.to || this.editedItem.domain == this.defaultItem.domain ){
+              alert("All values must be set");
+              return
+            }
+
+            if (this.editedIndex > -1) {
+                this.db.updateLink(this.links[this.editedIndex].id,this.editedItem)
+                Object.assign(this.links[this.editedIndex], this.editedItem)
+            } else {
+                this.db.insertLink(this.editedItem)
+
+                //Übergangslösung, es werden alle Daten neu geladen, effizienter wäre es nur das eine Object zu laden und dann im Array hinzuzufügen
+                // ist nötig weil Datenbank ID und Datum hinzufügt.
+                this.links = await this.initalize();  
+
+                //this.links.push(this.editedItem)
+            }
+            this.close()
+            },
+        },
+  }
+</script> 
+
